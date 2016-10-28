267963ff1a5a759306fc78e61417ce04c8bc5b9a
田中　暁 <satoru_tanaka@waku-2.com>
2016-10-28 18:23:43 +0900

    【add】mockとprod

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,46 @@ var gulp = require("gulp");
 var clean = require('gulp-clean');
 var server = require('gulp-webserver');
 var ejs = require('gulp-ejs');
+var pleeease = require('gulp-pleeease');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+var command = process.argv[2];
+
+/**
+ * buildかどうかチェックする
+ */
+var buildCheck = function () {
+  if (command == null) {
+    return command;
+  }
+  if (command === 'prod') {
+      return true;
+  } else {
+      return false;
+  }
+};
+
+/**
+ * パス
+ */
+var pass = (buildCheck()) ? {
+  top: 'prod',
+  view: 'prod/viewData',
+  css: 'prod/css',
+  js: 'prod/js',
+  img: 'prod/img',
+  lib: 'prod/lib',
+  fonts: 'prod/fonts'
+} : {
+  top: 'mock',
+  view: 'mock/viewData',
+  css: 'mock/css',
+  js: 'mock/js',
+  img: 'mock/img',
+  lib: 'mock/lib',
+  fonts: 'mock/fonts'
+}
 
 /**
  * 必要データを生成
@@ -76,7 +116,11 @@ var replaceHTML = function (htmlString) {
  * gulp.task
  */
 gulp.task('clean', function () {
-  gulp.src('prod/viewData/*').pipe(clean());
+  if (buildCheck()) {
+    gulp.src('prod/*').pipe(clean());
+  } else {
+    gulp.src('mock/*').pipe(clean());
+  }
 });
 
 gulp.task('ejs', ['view'], function () {
@@ -98,28 +142,71 @@ gulp.task('view', function () {
         if (index !== dirs.length - 1) {
           afterDir = dirs[index + 1];
         }
-        gulp.src('src/ejs/view/index.ejs').pipe(ejs({data: tmpData[index], directory: dir, beforeDir: beforeDir, afterDir: afterDir}, {ext: '.html'})).pipe(gulp.dest('prod/viewData/' + dir + '/'));
+        gulp.src('src/ejs/view/index.ejs').pipe(ejs({data: tmpData[index], directory: dir, beforeDir: beforeDir, afterDir: afterDir}, {ext: '.html'})).pipe(gulp.dest(pass.view + '/' + dir + '/'));
       });
-      gulp.src('src/ejs/index.ejs').pipe(ejs({data: tmpData}, {ext: '.html'})).pipe(gulp.dest('prod'));
+      gulp.src('src/ejs/index.ejs').pipe(ejs({data: tmpData}, {ext: '.html'})).pipe(gulp.dest(pass.top));
       });
   });
 });
 
+gulp.task('img', function () {
+  return gulp.src(['src/img/*', 'src/img/*.png'])
+    .pipe(gulp.dest(pass.img));
+});
+
 gulp.task('css', function () {
-  return gulp.src('src/css/*.css')
-  .pipe(gulp.dest('prod/css'));
+  if (buildCheck()) {
+    return gulp.src('src/css/*.css')
+      .pipe(pleeease())
+      .pipe(gulp.dest('prod/css'));
+  } else {
+    return gulp.src('src/css/*.css')
+      .pipe(gulp.dest('mock/css'));
+  }
 });
 
 gulp.task('js', function () {
-  return gulp.src('src/js/*.js')
-  .pipe(gulp.dest('prod/js'));
+  if (buildCheck()) {
+    return gulp.src('src/js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('prod/js'));
+  } else {
+    return gulp.src('src/js/*.js')
+      .pipe(gulp.dest('mock/js'));
+  }
 });
 
-gulp.task('serve', function () {
+gulp.task('lib', function () {
+  if (buildCheck()) {
+    return gulp.src(['src/css/lib/*.css'])
+      .pipe(pleeease())
+      .pipe(gulp.dest('prod/lib'));
+  } else {
+    return gulp.src(['src/css/lib/*.css'])
+      .pipe(gulp.dest('mock/lib'));
+  }
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('src/fonts/*')
+      .pipe(gulp.dest(pass.fonts));
+});
+
+gulp.task('mockServe', function () {
   // globで書き直し
   gulp.watch(['src/ejs/*.ejs', 'src/ejs/includes/common/*.ejs', 'src/ejs/includes/tmp/*.ejs', 'src/ejs/view/*.ejs'], ['ejs']);
   gulp.watch(['src/css/*.css'], ['css']);
   gulp.watch(['src/js/*.js'], ['js']);
+  gulp.src('mock')
+    .pipe(server({
+      host: '0.0.0.0',
+      port: 8808,
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('prodServe', function () {
   gulp.src('prod')
     .pipe(server({
       host: '0.0.0.0',
@@ -129,16 +216,25 @@ gulp.task('serve', function () {
     }));
 });
 
-gulp.task('prod', ['ejs', 'css', 'js'], function () {
-  console.log('prod完了');
+gulp.task('mock', ['ejs', 'css', 'js', 'img', 'lib', 'fonts'], function () {
+  console.log('mock compleate');
 });
+
+gulp.task('prod', ['ejs', 'css', 'js', 'img', 'lib', 'fonts'], function () {
+  console.log('build compleate');
+});
+
 
 gulp.task('dataClean', function () {
   gulp.src('src/tmpData/*').pipe(clean());
 });
 
+gulp.task('test', function () {
+  console.log(command);
+});
+
 gulp.task('default', function () {
   setTimeout(function () {
-    console.log(tmpData);
+    console.log(command);
   }, 100);
 });
