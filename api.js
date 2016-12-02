96870065f3903acf36e45d79675d4fa7391e8355
田中　暁 <satoru_tanaka@waku-2.com>
2016-12-02 17:30:33 +0900

    【fix】ifconfigが変わった

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ var token = require('./token');
 //evernote
 var Evernote = require('evernote').Evernote;
 
+console.log(token);
 var client = new Evernote.Client(token);
 
 module.exports = (function () {
@@ -17,7 +18,7 @@ module.exports = (function () {
   method.oauthTokenSecret = null;
 
   method.init = function (req, res) {
-    client.getRequestToken('http://192.168.179.2:3000/manager/'/*'http://10.17.208.153:3000/manager/'*/, function(err, oauthToken, oauthTokenSecret, results) {
+    client.getRequestToken(/*'http://192.168.179.2:3000/manager/'*/'http://10.17.209.13:3000/manager/', function(err, oauthToken, oauthTokenSecret, results) {
       // store tokens in the session
       if (err) {
         console.log(err);
