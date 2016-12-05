4f6106747ba8377d6fd9c178d8d9279c6063d3dc
st-0403-jp <sato252011@gmail.com>
2016-12-05 22:24:15 +0900

    【fix】tokenフォルダを作成

--- token.js
+++
@@ -1,7 +0,0 @@
-module.exports = (function () {
-    return {
-      consumerKey: 'sato252011-6631',// 'sato252011-7217',
-      consumerSecret: 'f0e91859ed625462',// '52dba2f462408a5d',
-      sandbox: false // Optional (default: true)
-    };
-})();
